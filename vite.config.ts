c9c5f3426aa677157c2554b9472f12462f01f249
marciosaraiva7 <marciohm7@gmail.com>
2024-02-08 11:48:55 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,40 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      devOptions: {
-        enabled: true,
+const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico"],
+  manifest: {
+    short_name: "ICNV Queimados",
+    name: "ICVN Queimados",
+    icons: [
+      {
+        src: "vite.svg",
+        sizes: "192x192",
+        type: "image/svg",
       },
-    }),
-  ],
+    ],
+    theme_color: "#ffffff",
+    background_color: "#ffffff",
+    display: "standalone",
+    scope: "/vite",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
+
+export default defineConfig({
+  plugins: [react(), VitePWA(manifestForPlugin)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 });
+
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
