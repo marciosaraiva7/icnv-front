7aa28690a32591510176704ab875cf54f04bef9a
marciosaraiva7 <marciohm7@gmail.com>
2024-02-08 11:31:14 -0300

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
