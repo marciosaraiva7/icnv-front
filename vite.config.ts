e5e877100b54dcc1486327e76e1f42abb92b89a6
marciosaraiva7 <marciohm7@gmail.com>
2024-02-14 09:40:01 -0300

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,33 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+// import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
-const manifestForPlugin: Partial<VitePWAOptions> = {
-  registerType: "prompt",
-  includeAssets: ["vite.svg"],
-  manifest: {
-    short_name: "ICNV Queimados",
-    name: "ICVN Queimados",
-    description: "",
-    icons: [
-      {
-        src: "vite.svg",
-        sizes: "192x192",
-        type: "image/svg",
-      },
-    ],
-    theme_color: "#ffffff",
-    background_color: "#ffffff",
-    display: "standalone",
-    scope: "/vite",
-    start_url: "/",
-    orientation: "portrait",
-  },
-};
+// const manifestForPlugin: Partial<VitePWAOptions> = {
+//   registerType: "prompt",
+//   includeAssets: ["vite.svg"],
+//   manifest: {
+//     short_name: "ICNV Queimados",
+//     name: "ICVN Queimados",
+//     description: "",
+//     icons: [
+//       {
+//         src: "vite.svg",
+//         sizes: "192x192",
+//         type: "image/svg",
+//       },
+//     ],
+//     theme_color: "#ffffff",
+//     background_color: "#ffffff",
+//     display: "standalone",
+//     scope: "/vite",
+//     start_url: "/",
+//     orientation: "portrait",
+//   },
+// };
 
 export default defineConfig({
-  plugins: [react(), VitePWA(manifestForPlugin)],
+  plugins: [react()],
   // build: {
   //   outDir: "./build-directory",
   // },
